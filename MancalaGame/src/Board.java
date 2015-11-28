28c222bbddb5dcde5e364c7f2c1fb133f33b4999
madhurigambhir <madhuri.gambhir@gmail.com>
2015-11-27 23:42:07 -0500

    Added basic game logic
    Game logic complete. Only AI part left

--- MancalaGame/src/Board.java
+++ MancalaGame/src/Board.java
@@ -2,7 +2,12 @@
 public class Board {
 	
 	private int[] board;
-	int boardSize;
+	
+
+	private int boardSize;
+	
+	
+	
 	
 	public Board(){}
 	
@@ -21,9 +26,15 @@ public class Board {
 			board[i] = numOfStones;
 		}
 		
+		
+		
 		//Set the Mancala pits to zero
 		board[size] = 0; //Mancala for player 1
 		board[lenOfArray-1] = 0; //Mancala for player 2
+		
+		/*for(int i=0; i<lenOfArray-1; i++){
+			System.out.print(board[i]);
+		}*/
 	}
 	
 	
@@ -31,30 +42,90 @@ public class Board {
 	 * Print the board
 	 */
 	
+	@Override
 	public String toString(){
-		String ret = new String();
-		for (int i : board){
-			ret = "i" + "," ;
-		}
 		
+		String ret = new String();
+		
+		/*for (int i : board){
+			ret = ret + "" + i + "," ;
+		}
 		//remove the last comma
 		return ret.substring(0,ret.length()-1);
+		*/
 		
+		System.out.print("P1 |  " +  board[getPitSize()] + " | ");
+		for(int i =getPitSize()-1; i >=0;i--){
+			System.out.print( board[i] + " ");
+			
+		}
+		System.out.print(" | ");
+		
+		System.out.println();
+		System.out.print("P2 |  " + " " + " | ");
+		
+		for(int i =1; i<=getPitSize();i++){
+			System.out.print(board[getPitSize() + i] + " ");
+		}
+		
+		System.out.print(" | ");
+		System.out.println(board[getBoardSize() -1]);
+		return "";
 	}
 	
-	public int getBoardSize() {
+	public int getPitSize() {
 		return boardSize;
 	}
-	
 
-	public void setBoardSize(int size) {
+	public void setPitSize(int size) {
 		this.boardSize = size;
 	}
 	
+	public int getBoardSize() {
+		return 2*boardSize + 2;
+	}
+	
+	/*
+	public void setBoardSize(int size) {
+		this.boardSize = size;
+	}*/
+	
 
+	public int[] getBoard() {
+		return board;
+	}
+
+	public void setBoard(int[] board) {
+		this.board = board;
+	}
+
+	
+	
 	//Display the board
 	public void displayBoard(){
 		System.out.println("Display the board");
+		System.out.println(toString());
+		
+	}
+	
+	
+	public int getMancala(int playerNum){
+		if (playerNum == 1){
+			return boardSize;
+		} else {
+			return (2*boardSize + 1);
+			
+		}
+	}
+	
+	//get opponents mancala
+	public int getOpponentsMancala(int playerNum){
+		if (playerNum == 2){
+			return boardSize;
+		} else {
+			return (2*boardSize + 1);
+			
+		}
 	}
 	
 	
--- MancalaGame/src/Mancala.java
+++ MancalaGame/src/Mancala.java
@@ -12,7 +12,7 @@ public class Mancala{
 		gameStart();
 	}
 
-	
+
 	//just to commit
 	//Preparation of game
 	public void gameStart(){
@@ -43,7 +43,7 @@ public class Mancala{
 
 			firstTurn = sc.nextInt();
 		}
-		
+
 		if (firstTurn == 3) {
 			System.exit(0);
 		} else if (firstTurn == 1){
@@ -51,8 +51,8 @@ public class Mancala{
 		} else{
 			player1 = new Player(false,1);
 		}
-		
-		
+
+
 		int secondTurn = -1;
 		while (secondTurn !=1 && secondTurn !=2){
 			System.out.println("Select the opponent");
@@ -69,11 +69,12 @@ public class Mancala{
 		} else{
 			player2 = new Player(false,2);
 		}
-		
-		
+
 		//TODO : Check for size of board
-		mancalaBoard = new Board(4,4);
-		
+		System.out.println("initialize the board");
+		mancalaBoard = new Board(4,3);
+
+
 		if (algoType == 1)
 			searchType = AlgoType.GREEDY;
 		else if (algoType == 2)
@@ -86,19 +87,43 @@ public class Mancala{
 
 	//Playing the game
 	public void playGame(){
-		
+
 		//display the board
-		mancalaBoard.toString();
+		//mancalaBoard.displayBoard();
 		Player currPlayer = player1;
+
 		while (true){
+
+			System.out.println("Playr Num is : " + currPlayer.playerNum);
+
 			Move nextMove;
 			if (!currPlayer.isHuman){
-				nextMove = getCompMove(mancalaBoard.getBoardSize(),searchType);
+				nextMove = getCompMove(mancalaBoard.getPitSize(),searchType);
 			} else {
-				nextMove = getHumanMove(mancalaBoard.getBoardSize(),currPlayer);
-				
+				//System.out.println("get human move");
+				nextMove = getHumanMove(mancalaBoard.getPitSize(),currPlayer);
+
 			}
-			
+
+			//Play the move
+			boolean gotFreeTurn = makeMove(nextMove,currPlayer);
+
+			if (isGameOver()){
+				getAllStones();
+				if (isGameDrawn()){
+					System.out.println("No winner : Game draw");
+				} else{
+					int winner = findWinner();
+					System.out.println("Game over. Player " + winner + " won.");
+					System.out.println("Scores are");
+					System.out.println("Player 1: " + getScore(player1));
+					System.out.println("Player 2: " + getScore(player2));
+
+				}
+
+				break;
+			}
+
 			if (wonPosition(nextMove, currPlayer.isHuman)){
 				if (currPlayer.isHuman){
 					System.out.println("Human won the Game");
@@ -107,44 +132,49 @@ public class Mancala{
 				}
 				//exit
 				System.exit(0);
-					
+
 			}
-			
-			if (drawnPosition(nextMove)){
-				System.out.println("Game draw");
-				//exit
-				System.exit(0);
-			}
-			
-			//if not won or draw make the new move 
+
 			//Toggle the turn if no free turn
-			if (!makeMove(nextMove)){
-				currPlayer = player2;
+			if (!gotFreeTurn){
+				if (currPlayer.equals (player1)){
+					currPlayer = player2;
+				}else {
+					currPlayer = player1; 
+				}
+
 			}
-			
+
 		}
-		
-    }
+
+	}
+
+
+
 
 
 
 	//Human move generator
 	public Move getHumanMove(int size, Player currPlayer){
 		mancalaBoard.displayBoard();
-		System.out.println(size);
+		//System.out.println(size);
 		int pitNum = -1;
-		while (pitNum < 0 || pitNum > size){
+		Move myMove = new Move();
+		do {
 			System.out.println("Select the pitnumber");
 			Scanner sc = new Scanner(System.in);
 			pitNum = sc.nextInt();
-			}
-		
-		Move myMove = covertIndexToMove(pitNum, currPlayer);
-		
-		System.out.println("Return to main");
+
+			myMove = convertIndexToMove(pitNum, currPlayer);
+
+		} while (pitNum<0 || pitNum > size || isIllegalMove(myMove));
+
+
+
+		//System.out.println("Return to main");
 		return myMove;
 	}
-	
+
 	//computer move generator
 	public Move getCompMove(int size, AlgoType searchType){
 		Move nextMove;
@@ -157,36 +187,71 @@ public class Mancala{
 		}
 		return nextMove;
 	}
-	
-	
-	//Generate nextMove according to greedy algorithm
-	public Move getGreedyMove(){
-		
-		return null;
-	}
-	
-	//Generate nextMove according to Minimax algorithm
-	public Move getMiniMaxMove(){
-		return null;
-	}
-	
-	//Generate nextMove according to aplha beta algorithm
-	public Move getAlphaBetaMove(){
-		return null;
-	}
-	
-	
+
+
+
+
 	//returns true if after making a turn we get a free turn
-	public boolean makeMove(Move move){
-	
-		return false;
+	public boolean makeMove(Move move, Player currPlayer){
+
+		boolean freeTurn = false;
+		int moveIndex = move.getMoveIndex();
+		int[] tempBoard = mancalaBoard.getBoard();
+		int boardSize = mancalaBoard.getBoardSize();
+		//System.out.println("moveIndex is: " +moveIndex);
+		int numOfStones = tempBoard[moveIndex];
+
+		tempBoard[moveIndex] = 0;
+		moveIndex++;
+
+		while(numOfStones>0){
+			//System.out.println("moveIndex is : " +  moveIndex);
+			//System.out.println("opo mancala is: " + mancalaBoard.getOpponentsMancala(currPlayer.playerNum));
+			if ((moveIndex % boardSize ) == 
+					mancalaBoard.getOpponentsMancala(currPlayer.playerNum)){
+				moveIndex++;
+				continue;
+			}
+			tempBoard[(moveIndex) % boardSize]++;
+			moveIndex++;
+			numOfStones--;
+		}
+
+		//if just to check if we get a free turn
+		int indexToCompare = (moveIndex-1) % boardSize;
+		int myMancala = mancalaBoard.getMancala(currPlayer.playerNum);
+
+
+		if (indexToCompare == myMancala)
+		{
+			freeTurn = true;
+		}else{
+
+			if (tempBoard[indexToCompare] == 1){
+				if ((currPlayer.playerNum == 1 && indexToCompare < mancalaBoard.getPitSize())
+						|| (currPlayer.playerNum == 2 && indexToCompare > mancalaBoard.getPitSize())){
+					//check opponents opp pit - 2*p - index
+					int oppPit = (mancalaBoard.getPitSize() * 2) - indexToCompare;
+					if (tempBoard[oppPit] > 0){
+						tempBoard[myMancala] += tempBoard[oppPit] + tempBoard[indexToCompare];
+						tempBoard[oppPit] = 0;
+						tempBoard[indexToCompare] = 0;
+					}
+				}
+			}
+		}
+
+		mancalaBoard.setBoard(tempBoard);
+		//System.out.println("freeturn is :"+ freeTurn);
+		return freeTurn;
 	}
-	
-	
+
+
+
 	//convert the index entered to a move
-	public Move covertIndexToMove(int pitNum,Player currPlayer){
+	public Move convertIndexToMove(int pitNum,Player currPlayer){
 		Move move;
-		int sizeOfBoard = mancalaBoard.getBoardSize();
+		int sizeOfBoard = mancalaBoard.getPitSize();
 		if (currPlayer.playerNum == 1){
 			//according to player1
 			move = new Move(pitNum-1);
@@ -194,12 +259,105 @@ public class Mancala{
 			//according to player2
 			move  = new Move(sizeOfBoard + pitNum);
 		}
-		
+
 		return move;
 	}
-	
-	
-	
+
+
+
+	public  boolean isGameOver(){
+
+		//check for player1
+		int[] tempBoard = mancalaBoard.getBoard();
+		int pitSize = mancalaBoard.getPitSize();
+		int totSize = mancalaBoard.getBoardSize();
+		boolean flag = true;
+
+		for(int i=0; i <pitSize;i++)
+		{
+			if (tempBoard[i] >0){
+				flag = false;
+				break;
+			}
+		}
+
+
+		//ch1eck for player2
+		if (!flag){
+			flag = true;
+			for(int j= pitSize+1; j< totSize-1; j++){
+				if (tempBoard[j] >0){
+					flag = false;
+					break;
+				}
+			}
+		}
+
+		if (!flag)
+			return false;
+		else 
+			return true;
+
+	}
+
+	public boolean isGameDrawn(){
+		if (getScore(player1) == getScore(player2))
+			return true;
+		else 
+			return false;
+	}
+
+
+	public void stealStones(){
+
+	}
+
+
+	private int findWinner() {
+		if (getScore(player1) > getScore(player2))
+			return 1;
+		else 
+			return 2;
+	}
+
+	public void getAllStones(){
+		int[] tempBoard = mancalaBoard.getBoard();
+		int pitSize = mancalaBoard.getPitSize();
+		int totSize = mancalaBoard.getBoardSize();
+
+		for(int i=0; i <pitSize;i++)
+		{
+			tempBoard[pitSize] += tempBoard[i];
+			tempBoard[i] = 0;
+		}
+
+
+		for(int j= pitSize+1; j< totSize-1; j++){
+			tempBoard[totSize-1] += tempBoard[j];
+			tempBoard[j] = 0;
+		}
+
+		mancalaBoard.setBoard(tempBoard);
+	}
+
+
+
+	//Generate nextMove according to greedy algorithm
+	public Move getGreedyMove(){
+
+		return null;
+	}
+
+	//Generate nextMove according to Minimax algorithm
+	public Move getMiniMaxMove(){
+		return null;
+	}
+
+	//Generate nextMove according to aplha beta algorithm
+	public Move getAlphaBetaMove(){
+		return null;
+	}
+
 	//The logic for greedy algorithm 
 	public void greedy(){
 
@@ -216,11 +374,6 @@ public class Mancala{
 	}
 
 
-	public boolean drawnPosition(Move move){
-		//check if i play the move is the game drawn
-		return false;
-	}
-
 	public boolean wonPosition(Move Move, boolean playerIsHuman){
 		//check if play the move is the game won
 		return false;
@@ -236,28 +389,19 @@ public class Mancala{
 		return null;
 	}
 
-	public Position makeMove(Position p, boolean playerIsHuman, Move move){
 
-		return null;
-	}
 
 	public boolean reachedMaxDepth(Position p, int depth){
 
 		return false;
 	}
 
-	public void distributeStones(){
 
 
-	}
 
-	public  boolean isGameOver(){
-
-		return false;
-	}
-
-	public int getScore(int player){
-		return 0;
+	public int getScore(Player currPlayer){
+		return (mancalaBoard.getBoard())[(mancalaBoard.getMancala
+				(currPlayer.playerNum))];
 	}
 
 	public void printPosition(){
@@ -265,6 +409,14 @@ public class Mancala{
 	}
 
 
+	//check if the move is illegal : if pit contains nothing 
+	public boolean isIllegalMove(Move myMove){
+
+		if (mancalaBoard.getBoard()[myMove.getMoveIndex()] == 0){
+			return true;
+		}
+		return false;
+	}
 
 	public static void main(String[] args){
 		Mancala playObj = new Mancala();
